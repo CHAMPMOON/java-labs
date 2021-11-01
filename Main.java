1d1c8b676fd37923a72a8b9840b85c17d372a992
Daniil <antipov8v@gmail.com>
2021-11-01 21:17:22 +0300

    create view main_view

--- Main.java
+++ Main.java
@@ -1,23 +1,11 @@
-import AviaryPackage.*;  // Пакет с вольерами
-import AnimalsPackage.*; // Пакет с животными
+import Model.AnimalsPack.*;  
+import Model.AviaryPack.*;  
+import Controller.MainController;
 
 public class Main 
 {
     public static void main(String[] args) 
     {
-        Aquarium aquarium = new Aquarium();
-        NetAviary netaviary = new NetAviary();
         
-        aquarium.SeeAnimals();
-        
-        WaterfowlAnimals fish = new WaterfowlAnimals("Fish", 12.3, 1);
-        
-        fish.Move(aquarium);
-        fish.Move(aquarium);
-        
-        fish.Move(netaviary);
-
-        aquarium.SeeAnimals();
-        netaviary.SeeAnimals();
     }
 }
